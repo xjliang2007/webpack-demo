a96afc3a5d03ced94f4f3c3e0b4ca2d559722ce0
小江 <xjliang2007@163.com>
2020-05-30 10:38:48 +0800

    learn dynamic generate index.html & webpack-dev

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 import _ from 'lodash';
-import './style.css'
-import Icon from './webpack.png'
-import Data from './data.xml'
+import printMe from './print'
 
 function component() {
   const element = document.createElement('div');
 
+  const btn = document.createElement('button');
+
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick= printMe;
 
-  element.appendChild(myIcon);
-  console.log(Data);
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,65 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin')
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-            'style-loader',
-            'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-            'file-loader'
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-            'file-loader'
-        ]
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-            'csv-loader'
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use: [
-            'xml-loader'
-        ]
-      }
-    ]
-  }
+	mode: "development",
+	entry: {
+		app: './src/index.js',
+		print: './src/print.js'
+	},
+    devtool: 'inline-source-map',
+	// devServer: {
+	// 	contentBase: './dist'
+	// },
+	plugins: [
+		new ManifestPlugin(),
+		new CleanWebpackPlugin({
+			cleanStaleWebpackAssets: false
+		}),
+		new HtmlWebpackPlugin({
+			title: 'Output Management'
+		})
+	],
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: "/"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test: /\.(csv|tsv)$/,
+				use: [
+					'csv-loader'
+				]
+			},
+			{
+				test: /\.xml$/,
+				use: [
+					'xml-loader'
+				]
+			}
+		]
+	}
 };
