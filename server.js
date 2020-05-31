12c9f8bdf9ea193637a4dc489d16389af13c5cc8
小江 <xjliang2007@163.com>
2020-05-31 16:31:00 +0800

    production learn

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const app = express();
-const config = require('./webpack.config');
+const config = require('./webpack.common');
 const compiler = webpack(config);
 
 app.use(webpackDevMiddleware(compiler,{
--- webpack.common.js
+++ webpack.common.js
@@ -4,15 +4,11 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin')
 
 module.exports = {
-	mode: "development",
 	entry: {
 		app: './src/index.js',
 		print: './src/print.js'
 	},
-    devtool: 'inline-source-map',
-	// devServer: {
-	// 	contentBase: './dist'
-	// },
+
 	plugins: [
 		new ManifestPlugin(),
 		new CleanWebpackPlugin({
