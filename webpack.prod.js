008a1529bf8ba2b9da14ecd22d053a90bd0f700c
小江 <xjliang2007@163.com>
2020-05-31 21:39:09 +0800

    provide offline service with service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,20 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
+const HtmlWebpackPlugin = require('Html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 module.exports = merge(common,{
 	mode: 'production',
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Progressive Web Application"
+		}),
+		new WorkboxPlugin.GenerateSW({
+			// these options encourage the ServiceWorkers to get in there fast
+			// and not allow any straggling "old" SWs to hang around
+			clientsClaim: true,
+	        skipWaiting: true,
+	     })
+	]
 })
