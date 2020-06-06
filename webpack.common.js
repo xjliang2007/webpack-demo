523e06f6aa4d8265a0de276a504f2c0ecf601667
小江 <xjliang2007@163.com>
2020-06-06 10:44:17 +0800

    test babel

--- webpack.common.js
+++ webpack.common.js
@@ -38,6 +38,11 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			},
+			{
 				test: /\.css$/,
 				use: [
 					'style-loader',
